e65a8ad0acbbd5dfbd947a6fc213bdc8f05a78bd
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-26 10:51:41 -0600

    Render basic messages

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const Message = require("../models/messageModel");
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Members only' });
+  // Get all messages and render them
+  Message.find()
+    .sort({ timeStamp: 1 })
+    .populate("poster")
+    .exec((err, messages) => {
+      if (err) {
+        return next(err);
+      }
+    res.render('index', { title: 'Members only', messages });
+    })
 });
 
 module.exports = router;
