b50b6c5c0671d6966df170295a45744c7fb822d5
Hector Gonzalez <gorozco.hector@gmail.com>
2022-11-27 18:02:57 -0600

    Only show author and date to members

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
+const { formatDistanceToNow } = require("date-fns");
 
-const Schema = mongoose.Schema;
+const { Schema }= mongoose;
 
 const MessageSchema = new Schema({
   title: {type: String, required: true, maxLength: 150},
   timeStamp: {type: Date, default: Date.now()},
   text: {type: String, required: true},
   poster: {type: Schema.Types.ObjectID, ref: "User", required: true},
+})
+
+MessageSchema.virtual("formattedTimeStamp").get(function () {
+  return formatDistanceToNow(this.timeStamp, { addSuffix: true});
 })
 
 module.exports = mongoose.model("Message", MessageSchema)
